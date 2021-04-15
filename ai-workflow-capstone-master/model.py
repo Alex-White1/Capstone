dd70f49ef1e3534d4509b4bbe55d2c06c924b16a
Alex-White1 <75363875+Alex-White1@users.noreply.github.com>
2021-04-15 14:24:08 +0100

    Add files via upload

--- ai-workflow-capstone-master/model.py
+++ ai-workflow-capstone-master/model.py
@@ -9,6 +9,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
+import xgboost as xgb
+from sklearn.model_selection import RandomizedSearchCV
+from xgboost import XGBRegressor
+
 
 from logger import update_predict_log, update_train_log
 from cslib import fetch_ts, engineer_features
@@ -25,7 +29,6 @@ def _model_train(df,tag,test=False):
     The 'test' flag when set to 'True':
         (1) subsets the data and serializes a test version
         (2) specifies that the use of the 'test' log file 
-
     """
 
 
@@ -58,7 +61,7 @@ def _model_train(df,tag,test=False):
     grid = GridSearchCV(pipe_rf, param_grid=param_grid_rf, cv=5, n_jobs=-1)
     grid.fit(X_train, y_train)
     y_pred = grid.predict(X_test)
-    eval_rmse =  round(np.sqrt(mean_squared_error(y_test,y_pred)))
+    eval_rmse = round(np.sqrt(mean_squared_error(y_test,y_pred)))
     
     ## retrain using all data
     grid.fit(X, y)
@@ -81,15 +84,58 @@ def _model_train(df,tag,test=False):
     ## update log
     update_train_log(tag,(str(dates[0]),str(dates[-1])),{'rmse':eval_rmse},runtime,
                      MODEL_VERSION, MODEL_VERSION_NOTE,test=True)
-  
 
-def model_train(data_dir,test=False):
+    ## train an XGBoost model
+    pipe_xgb = Pipeline(steps=[('st_scaler', StandardScaler()),
+                              ('xgb_model', xgb.XGBRegressor())])
+
+    param_grid_xgb = {
+        'xgb_model__subsample': np.arange(.05, 1, .05),
+        'xgb_model__max_depth': np.arange(3, 20, 1),
+        'xgb_model__colsample_bytree': np.arange(.1, 1.05, .05)
+    }
+
+    grid_xgb = RandomizedSearchCV(estimator=pipe_xgb, param_distributions=param_grid_xgb,
+                                  n_iter=10, scoring='neg_mean_squared_error', cv=4)
+
+    grid_xgb.fit(X_train, y_train)
+
+    y_pred_xgb = grid_xgb.predict(X_test)
+    eval_rmse_xgb = round(np.sqrt(mean_squared_error(y_test, y_pred_xgb)))
+
+    ## retrain using all data
+    grid_xgb.fit(X, y)
+    model_name = re.sub("\.", "_", str(MODEL_VERSION))
+    if test:
+        saved_model = os.path.join(MODEL_DIR,
+                                   "test-{}-{}.joblib".format(tag, model_name))
+        print("... saving test version of model: {}".format(saved_model))
+    else:
+        saved_model = os.path.join(MODEL_DIR,
+                                   "xgb-{}-{}.joblib".format(tag, model_name))
+        print("... saving model: {}".format(saved_model))
+
+    joblib.dump(grid_xgb, saved_model)
+
+    m, s = divmod(time.time() - time_start, 60)
+    h, m = divmod(m, 60)
+    runtime = "%03d:%02d:%02d" % (h, m, s)
+
+    ## update log
+    update_train_log(tag, (str(dates[0]), str(dates[-1])), {'rmse': eval_rmse_xgb}, runtime,
+                     MODEL_VERSION, MODEL_VERSION_NOTE, test=True)
+
+
+def model_train(data_dir=None,test=False):
     """
-    funtion to train model given a df
+    function to train model given a df
     
     'mode' -  can be used to subset data essentially simulating a train
     """
-    
+
+    if not data_dir:
+        data_dir = os.path.join("cs-train")
+
     if not os.path.isdir(MODEL_DIR):
         os.mkdir(MODEL_DIR)
 
@@ -107,7 +153,7 @@ def model_train(data_dir,test=False):
         if test and country not in ['all','united_kingdom']:
             continue
         
-        _model_train(df,country,test=False)
+        _model_train(df,country,test=test)
     
 def model_load(prefix='sl',data_dir=None,training=True):
     """
@@ -119,14 +165,14 @@ def model_load(prefix='sl',data_dir=None,training=True):
     if not data_dir:
         data_dir = os.path.join("cs-train")
     
-    models = [f for f in os.listdir(os.path.join(".","models")) if re.search("sl",f)]
+    models = [f for f in os.listdir('models') if re.search(prefix,f)]
 
     if len(models) == 0:
         raise Exception("Models with prefix '{}' cannot be found did you train?".format(prefix))
 
     all_models = {}
     for model in models:
-        all_models[re.split("-",model)[1]] = joblib.load(os.path.join(".","models",model))
+        all_models[re.split("-",model)[1]] = joblib.load(os.path.join("models",model))
 
     ## load data
     ts_data = fetch_ts(data_dir)
@@ -138,7 +184,7 @@ def model_load(prefix='sl',data_dir=None,training=True):
         
     return(all_data, all_models)
 
-def model_predict(country,year,month,day,all_models=None,test=False):
+def model_predict(query, all_models=None, all_data=None,test=False):
     """
     example funtion to predict from model
     """
@@ -148,22 +194,22 @@ def model_predict(country,year,month,day,all_models=None,test=False):
 
     ## load model if needed
     if not all_models:
-        all_data,all_models = model_load(training=False)
+        all_data, all_models = model_load(training=False, prefix=query['model'])
     
     ## input checks
-    if country not in all_models.keys():
+    if query['country'] not in all_models.keys():
         raise Exception("ERROR (model_predict) - model for country '{}' could not be found".format(country))
 
-    for d in [year,month,day]:
+    for d in [query['year'], query['month'], query['day']]:
         if re.search("\D",d):
             raise Exception("ERROR (model_predict) - invalid year, month or day")
     
     ## load data
-    model = all_models[country]
-    data = all_data[country]
+    model = all_models[query['country']]
+    data = all_data[query['country']]
 
     ## check date
-    target_date = "{}-{}-{}".format(year,str(month).zfill(2),str(day).zfill(2))
+    target_date = "{}-{}-{}".format(query['year'],str(query['month']).zfill(2),str(query['day']).zfill(2))
     print(target_date)
 
     if target_date not in data['dates']:
@@ -171,18 +217,18 @@ def model_predict(country,year,month,day,all_models=None,test=False):
                                                                                     data['dates'][0],
                                                                                     data['dates'][-1]))
     date_indx = np.where(data['dates'] == target_date)[0][0]
-    query = data['X'].iloc[[date_indx]]
+    final_query = data['X'].iloc[[date_indx]]
     
-    ## sainty check
+    ## sanity check
     if data['dates'].shape[0] != data['X'].shape[0]:
         raise Exception("ERROR (model_predict) - dimensions mismatch")
 
     ## make prediction and gather data for log entry
-    y_pred = model.predict(query)
+    y_pred = model.predict(final_query)
     y_proba = None
     if 'predict_proba' in dir(model) and 'probability' in dir(model):
         if model.probability == True:
-            y_proba = model.predict_proba(query)
+            y_proba = model.predict_proba(final_query)
 
 
     m, s = divmod(time.time()-time_start, 60)
@@ -190,10 +236,13 @@ def model_predict(country,year,month,day,all_models=None,test=False):
     runtime = "%03d:%02d:%02d"%(h, m, s)
 
     ## update predict log
-    update_predict_log(country,y_pred,y_proba,target_date,
+    update_predict_log(query['country'],y_pred,y_proba,target_date,
                        runtime, MODEL_VERSION, test=test)
-    
-    return({'y_pred':y_pred,'y_proba':y_proba})
+
+    if not y_proba:
+        return ({'y_pred': y_pred, 'y_proba': 'none'})
+    else:
+        return({'y_pred':y_pred,'y_proba':y_proba})
 
 if __name__ == "__main__":
 
@@ -201,20 +250,25 @@ if __name__ == "__main__":
     basic test procedure for model.py
     """
 
-    ## train the model
-    print("TRAINING MODELS")
-    data_dir = os.path.join("cs-train")
-    model_train(data_dir,test=True)
+    # ## train the model
+    # print("TRAINING MODELS")
+    # data_dir = os.path.join("cs-train")
+    # model_train(data_dir,test=False)
 
     ## load the model
     print("LOADING MODELS")
-    all_data, all_models = model_load()
+    all_data, all_models = model_load(prefix='xgb')
     print("... models loaded: ",",".join(all_models.keys()))
 
-    ## test predict
-    country='all'
-    year='2018'
-    month='01'
-    day='05'
-    result = model_predict(country,year,month,day)
-    print(result)
+    # ## test predict
+    # country='all'
+    # year='2018'
+    # month='01'
+    # day='05'
+    # result = model_predict(country,year,month,day)
+    # print(result)
+    
+    # ## test predict
+    # query={'country': 'all', 'year': '2018', 'month': '01', 'day': '05', 'model': 'xgb'}
+    # result = model_predict(query)
+    # print(result)
